ca03fbebfb74bec890de7b245273240518246193
Joseph Weissman <joe@bignerdranch.com>
2016-06-10 15:51:02 -0400

    build simple chains

--- lib/wordchain.rb
+++ lib/wordchain.rb
@@ -1,1 +1,68 @@
+require 'forwardable'
 require 'wordchain/version'
+
+module Wordchain
+  class Dictionary
+    extend Forwardable
+    def_delegators :entries, :size, :include?, :empty?
+
+    def entries
+      @entries ||= Dictionary.entries_from_system
+    end
+
+    def self.entries_from_system
+      File.
+        readlines('/usr/share/dict/words').
+        map(&:chomp)
+    end
+  end
+
+  class Chain
+    def initialize(start_with:, end_with:, dictionary:)
+      @dictionary = dictionary
+      @start_with = start_with
+      @end_with = end_with
+    end
+
+    def construct
+      current_word = @start_with
+      final_word = @end_with
+      chain = []
+
+      until current_word == final_word
+        chain << current_word
+        current_word = candidates_for(current_word).min_by { |w| letters_different(w, final_word) }
+      end
+
+      chain << current_word
+      chain
+    end
+
+    def candidates_for(word)
+      @dictionary.entries.select do |dict_word|
+        letters_different(word, dict_word) == 1
+      end
+    end
+
+    def letters_different(first_word, second_word)
+      differences = 0
+      first_word.chars.each_with_index do |ch, i|
+        if second_word.chars[i] != ch
+          differences += 1
+        end
+      end
+      differences
+    end
+
+    # def edit_distance_between(word_a, word_b)
+    # end
+
+
+    # def candidates_for(word)
+    #   # 1-edit distance away!
+    # end
+
+    # def edit_distance_between(word_a, word_b)
+    # end
+  end
+end
--- spec/wordchain_spec.rb
+++ spec/wordchain_spec.rb
@@ -1,8 +1,49 @@
+require 'pry'
 require 'spec_helper'
 require 'wordchain'
 
 describe Wordchain do
   it "should have a VERSION constant" do
     expect(subject.const_get('VERSION')).to_not be_empty
+  end
+end
+
+describe Dictionary do
+  subject(:dictionary) { Dictionary.new }
+
+  it { should_not be_empty }
+  it 'should have the expected number of entries' do
+    expect(dictionary.size).to eq(235_886)
+  end
+
+  it { should include('aardvark') }
+  it { should include('dirigible') }
+  it { should include('ringmaster') }
+  it { should include('hammerer') }
+end
+
+describe Chain do
+  subject(:chain) do
+    Chain.new(
+      dictionary: dictionary,
+      start_with: 'ruby', end_with: 'code'
+    ) 
+  end
+
+  context 'with a simple dictionary' do
+    let(:dictionary) do
+      instance_double(Dictionary, entries: %w[ cat bat bug rug star stag ruby rubs coal robs rods rode code cord ])
+    end
+
+    it 'should find candidates for next element of chain' do
+      expect(chain.candidates_for('cat')).to eql(['bat'])
+      expect(chain.candidates_for('bug')).to eql(['rug'])
+      expect(chain.candidates_for('star')).to eql(['stag'])
+      expect(chain.candidates_for('boat')).to eql([])
+    end
+
+    it 'should assemble the chain' do
+      expect(subject.construct).to eq(%w[ ruby rubs robs rods rode code ])
+    end
   end
 end
